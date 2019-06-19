4056d811beb205e5c6a10b1a378822ec7c83502e
nyaxs <nyaxs@outlook.com>
2019-06-19 20:48:55 +0800

    init&&add readme.md

---
+++ BreakTheBricksProject/Assets/Scripts/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    public float moveSpeed = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Debug.Log(h);
+        transform.Translate(new Vector3(h, v, 0)*Time.deltaTime*moveSpeed);
+	}
+}
